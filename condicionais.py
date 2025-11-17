0f7e6a4b747a3a962339baa01ec913c9e8cf06ad
Thanury Tanisue <thanurytanisue@gmail.com>
2025-11-17 11:19:21 -0300

    condicionais  if, elif e else

---
+++ condicionais.py
@@ -0,0 +1,14 @@
+# Condicionais: if, elif e else
+
+trabalho_terminado = False
+if trabalho_terminado == True:
+    print('Bora!')
+else:
+    print('Não posso sair')    
+
+valorA = int(input("Digite o primeiro: "))
+valorB = int(input("Digite o segundo valor:"))
+if valorA > valorB:
+    print(f'{valorA} é o Maior')
+else:
+    print(f'{valorB} é o Maior')    
